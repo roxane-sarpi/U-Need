8cd176a6e5a3b0be296ed18526e95b7a1cf8fa5b
Karenmarina <kareen_92@hotmail.fr>
2026-04-29 13:51:08 +0200

    reorganisation of the comments in the file router.js + renaming categoryController instead of CategoryController

--- backend/src/router.js
+++ backend/src/router.js
@@ -5,18 +5,20 @@ const router = express.Router();
 // const itemControllers = require("./controllers/itemControllers");
 
 // router.get("/items", itemControllers.browse);
-const adControllers =require("./controllers/adControllers");
-
-// router.get("/items", itemControllers.browse);
-router.get("/ads", adControllers.browse);
 // router.get("/items/:id", itemControllers.read);
 // router.put("/items/:id", itemControllers.edit);
 // router.post("/items", itemControllers.add);
 // router.delete("/items/:id", itemControllers.destroy);
 
-// module.exports = router;
+//Ads
+const adControllers =require("./controllers/adControllers");
 
-const CategoryControllers = require("./controllers/CategoryControllers");
+router.get("/ads", adControllers.browse);
+
+//Categories
+const CategoryControllers = require("./controllers/categoryControllers");
 
 router.get("/categories", CategoryControllers.browse);
+
+
 module.exports = router;
