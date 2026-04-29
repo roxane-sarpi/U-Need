6eee756c2404db91f3e33249ed5532a82a8fcd9b
Roxane Sarpi <roxane.sarpi@laplateforme.io>
2026-04-29 11:25:40 +0200

    update notifications model

---
+++ backend/src/models/NotificationsManager.js
@@ -0,0 +1,22 @@
+const AbstractManager = require("./AbstractManager");
+
+class NotificationsManager extends AbstractManager {
+  constructor() {
+    super({ table: "notifications" });
+  }
+
+    insert(notifications) {  
+  return this.database.query(`insert into ${this.table} (content, id_user) values (? , ?)`, 
+    [ notifications.content,  
+  notifications.id_user ]
+    );
+  }
+    update(notifications) {
+    return this.database.query(
+      `update ${this.table} set has_been_read = ? where id = ?`,
+      [notifications.has_been_read ?? false, notifications.id]
+    );
+  }
+}
+
+module.exports = NotificationsManager;
