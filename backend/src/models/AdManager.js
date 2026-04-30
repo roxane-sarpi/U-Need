b05472a8e96e799eabcef18d55c5fecadfd64626
Karenmarina <kareen_92@hotmail.fr>
2026-04-30 14:46:25 +0200

    update ad completly done

--- backend/src/models/AdManager.js
+++ backend/src/models/AdManager.js
@@ -25,7 +25,7 @@ class AdManager extends AbstractManager {
 
   update(ad) {
     return this.database.query(
-      `update ${this.table} set title = ? description = ? image_1 = ? image_2 = ? image_3 = ? id_category = ? points = ? statut = ? zip_code = ? city = ? urgent = ? date_execution = ? where id = ?`,
+      `update ${this.table} set title = ?, description = ?, image_1 = ?, image_2 = ?, image_3 = ?, id_category = ?, points = ?, statut = ?, zip_code = ?, city = ?, urgent = ?, date_execution = ? where id = ?`,
       [ad.title, ad.description, ad.image_1, ad.image_2, ad.image_3, ad.id_category, ad.points, ad.statut, ad.zip_code, ad.city, ad.urgent, ad.date_execution, ad.id]
     );
   }
