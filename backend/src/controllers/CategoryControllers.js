e9e9e486a77e918adf3107739cbd9fd6fca67ffa
geoffrey-badie <geoffrey.badie@laplateforme.io>
2026-05-04 09:59:00 +0200

    fonction destroy ajoutée

--- backend/src/controllers/CategoryControllers.js
+++ backend/src/controllers/CategoryControllers.js
@@ -42,8 +42,25 @@ const insert = (req, res) => {
     });
 };
 
+const destroy = (req, res) => {
+  models.category
+    .delete(req.params.id)
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
 module.exports = {
   browse,
   read,
-  insert
+  insert,
+  destroy,
 };
