9b5f42aa4214b5ef911d4d1dc098c61bde0b26fc
Roxane Sarpi <roxane.sarpi@laplateforme.io>
2026-05-04 09:22:39 +0200

    add destroy

--- backend/src/controllers/notificationsControllers.js
+++ backend/src/controllers/notificationsControllers.js
@@ -43,5 +43,21 @@ models.notification
   });
 };
 
-module.exports = {browse,add,edit};
+const destroy = (req, res) => {
+  models.notification
+    .delete(req.params.id)
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+module.exports = {browse,add,edit,destroy};
 
