d092e75a7fc59855b139d301d66c55a78bb2b3af
wendy-asmatico <wendy.asmatico@laplateforme.io>
2026-05-05 13:50:18 +0200

    change route

--- backend/src/router.js
+++ backend/src/router.js
@@ -26,13 +26,14 @@ router.get("/categories", CategoryControllers.browse);
 router.get("/categories/:id", CategoryControllers.read);
 
 
-const requestControllers = require("./controllers/requestControllers");
-router.post("/requests", requestControllers.addrequest);
+
 //Private routes
 
 // Authentication wall : verifyToken is activated for each route after this line
 router.use(verifyToken);
 
+const requestControllers = require("./controllers/requestControllers");
+router.post("/requests", requestControllers.addrequest);
 //Users
 router.get("/users/:id", userControllers.read);
 router.put("/users/:id", userControllers.edit);
