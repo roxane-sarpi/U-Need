3ca46c7fc298675bf13a5bdbb00743aa8498fd84
geoffrey-badie <geoffrey.badie@laplateforme.io>
2026-05-11 16:33:25 +0200

    brouillon page FAQ

---
+++ frontend/src/pages/FAQ.jsx
@@ -0,0 +1,10 @@
+export default function FAQ() {
+  return (
+<div tabIndex={0} className="collapse bg-pink-100 border-pink-300 border">
+  <div className="collapse-title font-semibold">How do I create an account?</div>
+  <div className="collapse-content text-sm">
+    Click the "Sign Up" button in the top right corner and follow the registration process.
+  </div>
+</div>
+  )
+}
