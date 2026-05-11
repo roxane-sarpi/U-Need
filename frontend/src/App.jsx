d42ca1ae6fe7624219728073f1f3cc92774073db
Karenmarina <kareen_92@hotmail.fr>
2026-05-11 16:28:32 +0200

    daisyui and tailwind config done and operational

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,26 @@
-import './App.css'
+import './index.css'
+
+/*Pour importer un style de notre fichier index.css : 
+<button className="btn" style={{ backgroundColor: "var(--color-accent)" }}>Buy Now</button>
+*/
 
 function App() {
-
+  //A modifer
   return (
-    <>
-     <p class="text-blue-600 dark:text-sky-400">The quick brown fox...</p>
-     <div role="alert" className="alert alert-info">
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="h-6 w-6 shrink-0 stroke-current">
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-  </svg>
-  <span>New software update available.</span>
+   <div className="card bg-base-100 w-96 shadow-sm">
+  <figure>
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+      alt="Shoes" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">Card Title</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div className="card-actions justify-end">
+      <button className="btn" style={{ backgroundColor: "var(--color-accent)" }}>Buy Now</button>
+    </div>
+  </div>
 </div>
-    </>
   )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+// import './index.css'   ← commenter ou supprimer
+// import './tailwind.css' ← commenter ou supprimer ici
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
