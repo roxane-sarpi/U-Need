d8b873cfa4027f8f6107166e22a84a62eac8754f
Karenmarina <kareen_92@hotmail.fr>
2026-05-18 14:14:21 +0200

    creation layout admin + layout public

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,3 @@
-import Header from './components/layout/Header'
-import Footer from './components/layout/Footer'
 import './index.css'
 import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -8,20 +6,32 @@ import EditProfile from './pages/Profile/EditProfile';
 import Login from './pages/Auth/Login';
 import Register from './pages/Auth/Register';
 import Notifications from './pages/Notifications';
+import MainLayout from './components/layout/MainLayout';
+import AdminLayout from './components/layout/AdminLayout';
+import Dashboard from './pages/Admin/Dashboard/Dashboard';
 
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
+    
       <Routes>
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/profile/edit" element={<EditProfile />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/notifications" element={<Notifications />} />
+        {/* Layout public */}
+        <Route element={<MainLayout />}>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile/edit" element={<EditProfile />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/notifications" element={<Notifications />} />
+        </Route>
+        
+        {/* Layout admin */}
+        <Route element={<AdminLayout />}>
+          <Route path="/admin" element={<Dashboard />} />
+        </Route>
+
       </Routes>
-      <Footer />
+
     </BrowserRouter>
   );
 }
---
+++ frontend/src/pages/Admin/Dashboard/Dashboard.jsx
@@ -0,0 +1,7 @@
+function Dashboard(){
+  return(
+        <div>Hey</div>
+  )
+}
+
+export default Dashboard;
