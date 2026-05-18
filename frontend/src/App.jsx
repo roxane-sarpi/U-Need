33938c7385d4d3cb59e6fc4ccb6d4bf671a23149
Theo-FERRETE <theo.ferrete@laplateforme.io>
2026-05-18 16:41:33 +0200

    about page with the GOAT team

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import Header from './components/layout/Header'
 import Footer from './components/layout/Footer'
 import './index.css'
-import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Profile from './pages/Profile/Profile';
 import EditProfile from './pages/Profile/EditProfile';
 import Login from './pages/Auth/Login';
 import Register from './pages/Auth/Register';
 import Notifications from './pages/Notifications';
+import About from './pages/About';
 
 
 function App() {
@@ -20,6 +20,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/notifications" element={<Notifications />} />
+        <Route path="/a-propos" element={<About />} />
       </Routes>
       <Footer />
     </BrowserRouter>
